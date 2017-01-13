29f0e2fa6fdaf590dbca13382eabd65f40feaf5e
Elizabeth <esimonian16@gmail.com>
2017-01-13 17:46:52 +0700

    set up tag pages

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,1 +1,10 @@
+$(document).ready(function() {
+  $('.set-1').addClass('chip')
+  $('.set-2').addClass('chip')
+  $('.set-3').addClass('chip')
+  $('.set-4').addClass('chip')
+  $('.set-5').addClass('chip')
+  $('a[rel=tag]').addClass('chip')
+  $('.post-tags').html().replace(/, /g , '')
+});
 
